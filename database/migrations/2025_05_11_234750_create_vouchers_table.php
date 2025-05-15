c81ec5ac04c7ccedc7b8d71c7395c3385477c719
ibldzn <haytsam.adzka@gmail.com>
2025-05-15 23:56:36 +0700

    feat: added voucher migration and model

---
+++ database/migrations/2025_05_11_234750_create_vouchers_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('vouchers', function (Blueprint $table) {
+            $table->id();
+            $table->string('voucher_code')->unique();
+            $table->string('voucher_name');
+            $table->double('voucher_minimum_transaction')->default(0);
+            $table->boolean('voucher_is_disabled')->default(false);
+            $table->string('voucher_description')->nullable();
+            $table->double('voucher_discount_percentage');
+            $table->dateTime('voucher_start_date')->default(now());
+            $table->dateTime('voucher_end_date')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('vouchers');
+    }
+};
