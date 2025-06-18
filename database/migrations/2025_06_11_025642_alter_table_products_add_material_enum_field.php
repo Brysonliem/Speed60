f6d93c29bd6e2d2503a57190ba97459df8368144
Haru-Kazumoto <jiaathaya@gmail.com>
2025-06-18 20:28:50 +0700

    fresh from cooler

--- database/migrations/2025_06_11_025642_alter_table_products_add_material_enum_field.php
+++ database/migrations/2025_06_11_025642_alter_table_products_add_material_enum_field.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('products', function(Blueprint $table) {
-            $table->enum('material', ['TITANIUM', 'STAINLESS'])->after('condition');
+            $table->enum('material', ['TITANIUM', 'STAINLESS','OTHER'])->after('condition');
         });
     }
 
